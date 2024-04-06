4367faf3f699f7a988142dbc54a39e728e57e65b
Brandon Hernandez Lopez <159638485+HLBrandon@users.noreply.github.com>
2024-04-06 12:50:30 -0600

    Delete php directory

--- php/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conexion = new mysqli('localhost', 'root', '', 'pase_lista_itsmt','3306');
-if ($conexion -> connect_error) {
-    print_r('ERROR AL CONECTAR: ' . $conexion -> connect_error);
-    exit();
-}
